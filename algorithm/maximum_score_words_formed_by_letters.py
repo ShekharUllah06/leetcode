150ed1989992eea058fd2c4d042e4b61e00ab2e2
Shekhar <aa1912275@gmail.com>
2022-11-30 10:45:22 +0600

    Solve some problem

--- algorithm/maximum_score_words_formed_by_letters.py
+++ algorithm/maximum_score_words_formed_by_letters.py
@@ -1,126 +1,22 @@
-
-# words = ["dog","cat","dad","good"]
-# #words=["xxxz", "ax", "bx", "cx"]
-# #words = ["leetcode"]
-# #score = [1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0]
-# #score=[4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 10]
-# score = [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1,
-#          0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-# letterMap={
-#     'a':0,
-#     'b':1,
-#     'c':2,
-#     'd':3,
-#     'e':4,
-#     'f':5,
-#     'g':6,
-#     'h':7,
-#     'i':8,
-#     'j':9,
-#     'k':10,
-#     'l':11,
-#     'm':12,
-#     'n':13,
-#     'o':14,
-#     'p':15,
-#     'q':16,
-#     'r':17,
-#     's':18,
-#     't':19,
-#     'u':20,
-#     'v':21,
-#     'w':22,
-#     'x':23,
-#     'y':24,
-#     'z':25,
-    
-# }
-
-
-# def makeSubset(A):
-#    # store all the sublists
-#    B = [[]]
-
-#    # first loop
-#    for i in range(len(A) + 1):
-#       # second loop
-#       for j in range(i + 1, len(A) + 1):
-#          # slice the subarray
-#          sub = A[i:j]
-#          B.append(sub)
-#    return B
-
-
-# myWordSubSets = makeSubset(words)
-# validSubSets=[]
-# for wordSet in myWordSubSets:
-#     #letters = ["z", "a", "b", "c", "x", "x", "x"]
-#     letters = ["l", "e", "t", "c", "o", "d"]
-#     err = 0
-#     for word in wordSet:
-#         for l in word:
-#            try:
-#                letters.remove(l)
-#            except ValueError:
-#                myWordSubSets.remove(wordSet)
-#                err=1
-#                break
-#         if err == 1:
-#             break
-#     if err==0:
-#         validSubSets.append(wordSet)
-# total=0
-# for wordSet in validSubSets:
-#     count=0
-#     for word in wordSet:
-#         for l in word:
-#             count = count+score[letterMap[l]]
-#     if count>total:
-#         total=count
-# print(total)
-
+import itertools
 class Solution(object):
-    def makeSubset(self, A):
-        B = [[]]
-        for i in range(len(A) + 1):
-            for j in range(i + 1, len(A) + 1):
-                sub = A[i:j]
-                B.append(sub)
-        return B
+    def findsubsets(self,s, n):
+        return list(itertools.combinations(s, n))
+    
+    def makeallsubsets(self, s):
+        length=len(s)
+        SUBSETS=[[]]
+        for i in range(1,length+1):
+            sub = self.findsubsets(s,i)
+            for x in sub:
+                SUBSETS.append(list(x))
+        return SUBSETS
+        
 
     def maxScoreWords(self, words, letters, score):
-        letterMap = {
-            'a': 0,
-            'b': 1,
-            'c': 2,
-            'd': 3,
-            'e': 4,
-            'f': 5,
-            'g': 6,
-            'h': 7,
-            'i': 8,
-            'j': 9,
-            'k': 10,
-            'l': 11,
-            'm': 12,
-            'n': 13,
-            'o': 14,
-            'p': 15,
-            'q': 16,
-            'r': 17,
-            's': 18,
-            't': 19,
-            'u': 20,
-            'v': 21,
-            'w': 22,
-            'x': 23,
-            'y': 24,
-            'z': 25,
-        }
-        myWordSubSets = self.makeSubset(words)
+        myWordSubSets = self.makeallsubsets(words)
         validSubSets = []
         for wordSet in myWordSubSets:
-            print(wordSet)
             ltrSet = letters[:]
             err = 0
             for word in wordSet:
@@ -128,7 +24,6 @@ class Solution(object):
                     try:
                         ltrSet.remove(l)
                     except ValueError:
-                        myWordSubSets.remove(wordSet)
                         err = 1
                         break
                 if err == 1:
@@ -137,21 +32,23 @@ class Solution(object):
                 validSubSets.append(wordSet)
                 
         total = 0
-        print(validSubSets)
         
         for wordSet in validSubSets:
             count = 0
             for word in wordSet:
                 for l in word:
-                    count = count+score[letterMap[l]]
+                    pos = ord(l)-97
+                    count = count+score[pos]
             if count > total:
                 total = count
         return (total)
 
 obj= Solution()
-result = obj.maxScoreWords(["get", "set"],
-                           ["g", "s", "e", "t"],
-                           [0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0]
+result = obj.maxScoreWords(["baaa", "aacc", "ccbc", "da", "dbbc"],
+                           ["a", "a", "a", "c", "c", "c", "c", "c", "d", "d", "d"],
+                           [9, 4, 10, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
                   )
 print(result)
+
+
